0bc9f14443b134d4ea588ff793be96108bd26c11
Shuai99999 <gaoshuai099@gmail.com>
2024-01-28 10:00:33 +0800

    the guess number game

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,8 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const x = '23';
-if (x === 23) console.log(23);
+// const x = '23';
+// if (x === 23) console.log(23);
 
-const calcAge = birhtYear => 2037 - birhtYear;
-console.log(x);
+// const calcAge = birhtYear => 2037 - birhtYear;
+// console.log(x);
+
+const arr1 = [17, 21, 23];
+const arr2 = [12, 5, -5, 0, 4];
+
+function printForecast(arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `...${arr[i]}℃ in ${i + 1} days`;
+    // console.log(`...${arr[i]}℃ in ${i + 1} days`);
+  }
+  str = str + '...';
+  console.log(str);
+}
+
+printForecast(arr1);
+printForecast(arr2);
