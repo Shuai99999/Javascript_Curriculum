46fff877dd8921460cba2ada3af4e7fbc979fa74
Shuai99999 <gaoshuai099@gmail.com>
2024-03-09 16:58:52 +0800

    1

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -155,6 +155,21 @@ const updateUI = function (acc) {
 // Event handlers
 let currentAccount;
 
+// FAKE ALWAYS LOGGED In
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+const now = new Date();
+const day = `${now.getDate()}`.padStart(2, 0);
+// 月份是从0开始的 所以要+1
+const month = `${now.getMonth() + 1}`.padStart(2, 0);
+const year = now.getFullYear();
+const hour = `${now.getHours()}`.padStart(2, 0);
+const min = `${now.getMinutes()}`.padStart(2, 0);
+
+labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
   e.preventDefault();
@@ -251,3 +266,166 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+/*
+
+console.log(23 === 23.0);
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// 强转字符为数字的方法
+console.log(Number('23'));
+console.log(+'23');
+
+// Parsing转换方法
+// 必须以数字开头，第二个以字母开头会失败，使用场景是可以方便地把数字的单位去掉
+// 后面的参数10代表10进制
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23', 10));
+
+// 带空格也能转，int的结果不带.5只保留整数部分，float结果为2.5
+console.log(Number.parseInt(' 2.5rem '));
+console.log(Number.parseFloat(' 2.5rem '));
+
+// 这两个函数为global function 因此也可以单独使用
+console.log(parseInt(' 2.5rem '));
+console.log(parseFloat(' 2.5rem '));
+
+// 判断是否为非数字，默认会强转为数字，所以第二行也会显示为false
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
+
+// 与上面的操作相反，判断是否为数字
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
+
+// 判断是否为整数
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
+
+// 开平方和开立方
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+// 取最大值
+console.log(Math.max(5, 18, 23, 11, 2));
+// 这里的'23'可以强转为数字
+console.log(Math.max(5, 18, '23', 11, 2));
+// 23px就不行了
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// 生成1-6之间的数
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+// 生成指定大小范围内的随机数
+const randomInt = (min, max) =>
+  // 这里用floor代替trunc以便更好地适应负数的情况
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(10, 20));
+
+console.log(Math.trunc(23.3));
+
+// 四舍五入
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+// 向上取整
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+// 向下取整，且可以强转
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+
+// 对于负数也可以这样操作，且负数floor可以智能地向更小的负数方向取整
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+// 按指定小数位数保留小数，默认生成的结果都是字符了而不是数字
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+// 这样加个强转就是返回数字类型的值了
+console.log(+(2.345).toFixed(2));
+
+// 求余
+console.log(5 % 2);
+
+// 判断是否为偶数
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(0));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
+
+// 科学计数法表示法，这个下划线符号位置随便写，不影响运算结果，只是为了提高可读性
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+// bigInt类型数字
+// 普通int只有二进制53位，超过了就很难正确表示这个数字
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+
+// 以下两种方式可以表示bigInt
+console.log(83485769385236485234752398457234n);
+// 其中下面这个方法不能放太长的数字在里面，可能是因为它还是先是int再强转成BigInt的，所以在int的时候它已经失真，如果里面放一个小一些的数是不会失真的
+console.log(BigInt(83485769385236485234752398457234));
+
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Mar 08 2024 15:25:45'));
+console.log(new Date('December 24, 2015'));
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+// 0是1970年1月1日，前面*3就是3天后，*1000是以ms为单位
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142228180000));
+console.log(Date.now());
+
+// 除了get还有set方法，可以修改时间中的某个部分，这里只演示年
+future.setFullYear(2040);
+console.log(future);
+
+*/
