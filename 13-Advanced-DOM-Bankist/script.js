e0accfb2052644ac2c3deb6b74b1c4d98e0a2ab3
Shuai99999 <gaoshuai099@gmail.com>
2024-03-14 19:49:14 +0800

    1

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -29,3 +29,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// 创建和插入元素
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookie for improved functionality and analytics.';
+
+message.innerHTML =
+  'We use cookie for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>';
+
+// 将message放在header的开头
+header.prepend(message);
+// 将message放在header的结尾
+// header.append(message);
+// 复制message
+// header.append(message.cloneNode(true));
+
+// 将message放在header的之前，这个是放在header外面了，上面的pend是放在header里面
+// header.before(message);
+// 将message放在header的之后
+// header.after(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // 这两种方法都能删除这个元素
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
