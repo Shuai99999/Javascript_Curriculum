c4477a4c8c7fd1dd86790ef417b4cfe7597381b0
Shuai99999 <gaoshuai099@gmail.com>
2024-03-21 21:27:36 +0800

    1

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -33,3 +33,53 @@ jonas.calcAge();
 matilda.calcAge();
 
 console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+// Person.prototype是否是他们仨的原型，最后一个不是
+// 因为，jonas的原型是Person.prototype的构造函数的资产（property）
+// 构造函数和new出来的对象之间就是通过__proto__相关联的
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+// 第二个是false，因为只有明确在构造函数中写明的属性才算true，而species是在prototype中加入的，相当于是隐含的属于他们的一个属性了
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+console.log(jonas.__proto__);
+// 这里相当于是prototype的chain，可以看到前面引用过的hasOwnProperty等属性
+console.log(jonas.__proto__.__proto__);
+// 再来一层结果就是null了，上一层已经到头了
+console.log(jonas.__proto__.__proto__.__proto__);
+
+// 这个构造函数属性，当然就是把函数本身打印出来了
+console.log(Person.prototype.constructor);
+// 这样可以看到更详尽的属性，包括后来隐式定义的species等
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+// 这里数组的所有方法都被打印出来
+console.log(arr.__proto__);
+// 它俩相等，还是那个原理，数组的prototype属性等于所有被它创造出来的对象（也就是具体的新数组）的原型
+console.log(arr.__proto__ === Array.prototype);
+
+// 这里就是prototype的方法了，套娃
+console.log(arr.__proto__.__proto__);
+
+// 例如，这里直接改写Array的原型，加个unique方法
+Array.prototype.unique = function () {
+  // 这里的this即代表后续要调用它的那个函数
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+// 这里展开可以看到HTML元素的各个节点，即DOM的解构，可以往下6-7层，每一层都有自己的prototype，然后继续往下展开
+console.dir(h1);
+
+// 随便写个函数，打印它的dir，可以看到与函数有关的各种方法和原型，例如apply bind call
+console.dir(x => x + 1);
