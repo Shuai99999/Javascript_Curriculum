ec8b0e76dea6cb830c1eb25267952839afa43dc4
Shuai99999 <gaoshuai099@gmail.com>
2024-06-24 18:36:13 +0800

    1

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -4,6 +4,12 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
 const renderCountry = function (data, className = '') {
   const html = `
   <article class="country ${className}">
@@ -14,38 +20,41 @@ const renderCountry = function (data, className = '') {
       <p class="country__row"><span>👫</span>${(
         +data.population / 1000000
       ).toFixed(1)}</p>
-      <p class="country__row"><span>🗣️</span>${Object.values(
-        data.languages
-      )}</p>
+        <p class="country__row"><span>🗣️</span>${Object.values(
+          data.languages
+        )}</p>
       <p class="country__row"><span>💰</span>${
         Object.values(data.currencies)[0]['name']
       }</p>
-    </div>
-  </article>`;
+      </div>
+      </article>`;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
+  // 这句放在捕获异常的finally里了
+  // 后来在做challeng1的时候又comment out出来了
   countriesContainer.style.opacity = 1;
 };
 
+/*
 const getCountryAndNeighbour = function (country) {
   const request = new XMLHttpRequest();
-
+  
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
-
+  
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-
+    
     // render country 1
     renderCountry(data);
-
+    
     // get neighbour country (2)
     const [neighbour] = data.borders;
     console.log(neighbour);
 
     if (!neighbour) return;
-
+    
     // Ajax call country 2
     const request2 = new XMLHttpRequest();
     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
@@ -58,5 +67,235 @@ const getCountryAndNeighbour = function (country) {
   });
 };
 
-getCountryAndNeighbour('portugal');
+// getCountryAndNeighbour('portugal');
 // getCountryAndNeighbour('usa');
+
+
+// const getCountryData = function (country) {
+  //   fetch(`https://restcountries.com/v3.1/name/${country}`)
+  //     .then(function (response) {
+    //       console.log(response);
+    //       // 上面的response打印出来就是我们需要的数据，它的数据类型是response
+    //       // 下面这个response.json还是一个promise，老师也不知道为什么这样设计，总之就是这样
+    //       // 由于它是promise类型，因此再往下还可以继续then
+//       return response.json();
+//     })
+//     .then(function (data) {
+  //       // 到这里，data就是array类型了
+  //       console.log(data);
+  //       renderCountry(data[0]);
+  //     });
+  // };
+  
+  // 以下为简洁写法，去掉了打印日志并使用箭头函数
+  // AJAX的promise的写法总体更简洁，取代了之前的XMLHpptRequest的open send和后面的addEventListener等大量代码
+  
+  const getJSON = function (url, errorMsg = 'Something went wrong') {
+    // 注意下面这个return，必须加上它，这个函数才相当于是返回一个promise，否则后续调用它的时候会无返回值而报错
+    return fetch(url).then(response => {
+      if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+    return response.json();
+  });
+};
+
+// const getCountryData = function (country) {
+  //   // Country 1
+  //   fetch(`https://restcountries.com/v3.1/name/${country}`)
+  //     .then(response => {
+    //       // console.log(response);
+    //       // 这里的throw error仅是针对这一步抛出自动以的错误信息，避免直接抛出后台错误，显得不安全，其中response.status就是404等响应状态码
+//       // 这个错误最终是被传递到catch部分被抛出的
+//       if (!response.ok)
+//         throw new Error(`Country not found (${response.status})`);
+//       return response.json();
+//     })
+//     .then(data => {
+  //       renderCountry(data[0]);
+  //       const neighbour = data[0].borders[0];
+  //       if (!neighbour) return;
+  
+  //       // Country 2
+//       return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     })
+//     // 注意，下面的then不要写在上面的return后面，而是要写在外面一层大括号圆括号外，否则又变成callback hell了
+//     .then(response => response.json())
+//     .then(data => {
+//       renderCountry(data[0], 'neighbour');
+//     })
+//     // 这里的catch是可以捕获上面所有的错误的，相当于一个全局的catch
+//     .catch(err => {
+  //       console.error(`${err} 🧨🧨🧨`);
+//       renderError(`Something went wrong 🧨🧨 ${err.message}. Try again!`);
+//     })
+//     // finally是不管上面的promise执行正确还是错误都会被执行的部分
+//     .finally(() => {
+  //       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+const getCountryData = function (country) {
+  // Country 1
+  getJSON(`https://restcountries.com/v3.1/name/${country}`, 'Country not found')
+  .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) throw new Error('No neighbour found!');
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
+    })
+    // 注意，下面的then不要写在上面的return后面，而是要写在外面一层大括号圆括号外，否则又变成callback hell了
+    .then(data => {
+      renderCountry(data[0], 'neighbour');
+    })
+    // 这里的catch是可以捕获上面所有的错误的，相当于一个全局的catch
+    .catch(err => {
+      console.error(`${err} 🧨🧨🧨`);
+      console.log(err);
+      renderError(`Something went wrong 🧨🧨 ${err.message}. Try again!`);
+    })
+    // finally是不管上面的promise执行正确还是错误都会被执行的部分
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+  };
+
+  btn.addEventListener('click', function () {
+    getCountryData('portugal');
+  });
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+
+// In this challenge you will build a function 'whereAmI' which renders a country ONLY based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+// Here are your tasks:
+
+// PART 1
+// 1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
+// 2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates to a meaningful location, like a city and country name. Use this API to do reverse geocoding: https://geocode.xyz/api.
+// The AJAX call will be done to a URL with this format: https://geocode.xyz/52.508,13.381?geoit=json. Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, that is cheating 😉
+// 3. Once you have the data, take a look at it in the console to see all the attributes that you recieved about the provided location. Then, using this data, log a messsage like this to the console: 'You are in Berlin, Germany'
+// 4. Chain a .catch method to the end of the promise chain and log errors to the console
+// 5. This API allows you to make only 3 requests per second. If you reload fast, you will get this error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+// PART 2
+// 6. Now it's time to use the received data to render a country. So take the relevant attribute from the geocoding API result, and plug it into the countries API that we have been using.
+// 7. Render the country and catch any errors, just like we have done in the last lecture (you can even copy this code, no need to type the same code)
+
+// TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+// TEST COORDINATES 2: 19.037, 72.873
+// TEST COORDINATES 2: -33.933, 18.474
+
+// GOOD LUCK 😀
+
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+  .then(function (response) {
+    // console.log(response);
+      if (!response.ok)
+      throw new Error(`Problem with geocoding ${response.status}`);
+    // if (!response.redirected)
+    //   throw new Error(`You are calling too fast! ${response.status}`);
+      return response.json();
+    })
+    .then(function (data) {
+      if (data.city.includes('Throttled'))
+      throw new Error('You are calling too fast!');
+    console.log(`You are in ${data.city}, ${data.country}`);
+    return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then(function (response) {
+      if (!response.ok)
+      throw new Error(`Country not found (${response.status})`);
+      return response.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => {
+      console.log(err.message);
+    });
+};
+
+whereAmI(52.508, 13.381);
+whereAmI(19.037, 72.873);
+whereAmI(-33.933, 18.474);
+
+
+// Event Loop in Practice
+// JS只有单进程没有并发，所有事件会按一定顺序扔到Event Loop里顺序执行
+// 其中像AJAX的Promise是Mirco event loop，它具有更高的优先级，只要不断有新的Mirco event loop插进来，理论上它就会一直阻塞排在它后面的正常的Event Loop事件
+// 以下是Mirco event loop的例子
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i++; i < 100000000000000000) {}
+  console.log(res);
+});
+
+console.log('Test end');
+
+
+// new Promise是个标准的方法，它有通过resolve和拒绝reject两个输入
+// 也可以像下面两行这样单独使用
+// Promise.resolve('abc').then(x => console.log(x));
+// Promise.reject(new Error('Problem!')).catch(x => console.error(x));
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lotter draw is happening! ✨');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 🧧');
+    } else {
+      reject(new Error('You lost your money 🎃'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+*/
+// Promisfying setTimeout
+// 将原本这样写的timeout函数，用primise改写，这样可读性更强一点，不用套一层一层的了
+// setTimeout(() => {
+//   console.log('I waited for 1 seconds');
+//   setTimeout(() => {
+//     console.log('I waited for 2 seconds');
+//     setTimeout(() => {
+//       console.log('I waited for 3 seconds');
+//       setTimeout(() => {
+//         console.log('I waited for 4 seconds');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(1)
+  .then(() => {
+    console.log('I waited for 1 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 3 seconds');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('I waited for 4 seconds');
+    return wait(1);
+  });
