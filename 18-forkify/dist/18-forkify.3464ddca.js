d760c471e877df8d27d5c20f5f46cc9fc2b1702c
Shuai99999 <gaoshuai099@gmail.com>
2024-08-04 09:33:22 +0800

    1

--- 18-forkify/dist/18-forkify.3464ddca.js
+++ 18-forkify/dist/18-forkify.3464ddca.js
@@ -142,7 +142,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"hAbKD":[function(require,module,exports) {
+})({"4VPge":[function(require,module,exports) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
@@ -585,6 +585,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"j4kuM":[function(require,module,exports) {
 
-},{}]},["hAbKD","j4kuM"], "j4kuM", "parcelRequire3a11")
+},{}]},["4VPge","j4kuM"], "j4kuM", "parcelRequire3a11")
 
 //# sourceMappingURL=18-forkify.3464ddca.js.map
